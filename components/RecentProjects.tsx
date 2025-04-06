a6237ff0ae669ddcbdd294d8433d4f1ff2c7bd4b
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-04-07 00:15:06 +0530

    bug fix of 3d cards
    bug fix of 3d cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -25,19 +25,19 @@ const RecentProjects = () => {
               title="Explore"
               href=""
             >
-              {/* <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                {/* <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
                   <img src="/bg.png" alt="bgimg" />
-                </div>
+                </div> */}
                 <img
                   src={item.img}
                   alt="cover"
                   className="z-10 absolute bottom-0"
                 />
-              </div> */}
+              </div>
 
               <h2 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1 text-white">
                 {item.title}
