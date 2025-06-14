9d38929dd62853b11bb8286282582a65e7661f7d
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-06-14 16:02:25 +0530

    formating
    formating

--- data/index.ts
+++ data/index.ts
@@ -16,29 +16,7 @@ interface GridItem {
   spareImg?: string;
 }
 
-// export const gridItems = [
-//   {
-//     id: 1,
-//     title: "I prioritize client collaboration, fostering open communication",
-//     description: "",
-//     className: "lg:col-span-2 md:col-span-2 lg:row-span-2 lg:min-h-[30vh]",
-//     imgClassName: "w-full h-full",
-//     titleClassName: "justify-end",
-//   },
-//   {
-//     id: 2,
-//     title: "I'm very flexible with time zone communications",
-//     description: "",
-//     className: "sm:col-span-1 md:col-span-1 md:row-span-1",
-//   },
-//   {
-//     id: 3,
-//     title: "My tech stack",
-//     description: "I constantly try to improve",
-//     className: "sm:col-span-1 md:col-span-1 md:row-span-1",
-//   },
 
-// ];
 
 export const projects = [
   {
